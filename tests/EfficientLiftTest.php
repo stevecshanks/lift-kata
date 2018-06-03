79d724b2a9f4cc6a8ad4cfadb4d8caf724b4fb65
Steve Shanks <steve.c.shanks@gmail.com>
2018-06-03 10:01:34 +0100

    Add a more complex example for Efficient Lift

--- tests/EfficientLiftTest.php
+++ tests/EfficientLiftTest.php
@@ -29,4 +29,36 @@ class EfficientLiftTest extends TestCase
             $this->assertLessThanOrEqual(2, $lift->getNumberOfVisits($floor));
         }
     }
+
+    public function testSomeMorePeopleAreTakenToTheirDestinationEfficiently()
+    {
+        $this->markTestIncomplete(); // TODO remove this line!
+        
+        $people = [
+            new Person(0, 6),
+            new Person(0, 2),
+            new Person(0, 2),
+            new Person(1, 3),
+            new Person(1, 5),
+            new Person(3, 4),
+            new Person(3, 5),
+            new Person(3, 6),
+            new Person(3, 7),
+            new Person(5, 2),
+            new Person(5, 2),
+            new Person(7, 1),
+            new Person(7, 0),
+            new Person(7, 2),
+            new Person(7, 6),
+        ];
+
+        $lift = new EfficientLift();
+        $lift->movePeople($people);
+
+        $this->assertPeopleHaveArrivedAtTheirDestinations($people);
+
+        foreach (range(0, 7) as $floor) {
+            $this->assertLessThanOrEqual(2, $lift->getNumberOfVisits($floor));
+        }
+    }
 }
