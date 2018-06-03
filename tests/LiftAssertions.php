0313b323305cd04562dd8f504fa57a96f6802b3c
Steve Shanks <steve.c.shanks@gmail.com>
2018-06-03 09:59:12 +0100

    Clearer assertion message

--- tests/LiftAssertions.php
+++ tests/LiftAssertions.php
@@ -12,7 +12,15 @@ trait LiftAssertions
     protected function assertPeopleHaveArrivedAtTheirDestinations(array $people)
     {
         foreach ($people as $person) {
-            $this->assertSame($person->getDestination(), $person->getCurrentFloor());
+            $this->assertSame(
+                $person->getDestination(),
+                $person->getCurrentFloor(),
+                sprintf(
+                    'Expected person to be on floor %d but they are actually on floor %d',
+                    $person->getDestination(),
+                    $person->getCurrentFloor()
+                )
+            );
         }
     }
 }
